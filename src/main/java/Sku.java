0a277b82f350603a5bfd2b7f91a4e6965d45fb62
pakhomn <tasha.pakhom@gmail.com>
2023-06-30 08:26:46 +0300

    Комит 1 для консольного приложения

---
+++ src/main/java/Sku.java
@@ -0,0 +1,12 @@
+public class Sku {
+    String name; //название товара
+    double price; //цена товара
+    public static int goodsCount;
+
+    //теперь делаем  конструктор для товара
+    Sku(String nameSKU, double priceSKU){
+        name = nameSKU;
+        price = priceSKU;
+        goodsCount++;
+    }
+}
