a77fa2e559d553bbf12aba17aecc8a635fb181c4
pakhomn <tasha.pakhom@gmail.com>
2023-07-02 18:07:34 +0300

    Исправления - коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,39 +17,16 @@ public class Main {
          */
 
         int k;
-        System.out.println("Укажите, на скольких человек делим счет:");
-        if (scanner.hasNextInt()) {
+        do {
+            System.out.println("Укажите, на скольких человек делим счет.\nВыберете значение больше 1:");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Напишите значение целым числом, не используйте буквы.");
+                scanner.next();
+            }
             k = scanner.nextInt();
-            while (k <= 1) {
-                System.out.println("Количество человек должно быть больше 1, укажите другое значение:");
-                if (scanner.hasNextInt()) {
-                    k = scanner.nextInt();
-                } else {
-                    System.out.println("Укажите количество целой цифрой, без этого калькулятор не будет работать."); return;
-                }
-            }
-            System.out.println("Замечательно! Количество человек - " + k +"\n\nТеперь укажи товары и их цены. Или напиши 'завершить' для рассчета.");
-        } else {
-            System.out.println("Укажите количество целой цифрой, без этого калькулятор не будет работать."); return;
-        }
+        } while (k <= 1);
+        System.out.println("Замечательно! Количество человек - " + k +"\n\nТеперь укажи товары и их цены. Или напиши 'завершить' для рассчета.");
 
-
-        /*
-        класс - в итоге вынесла отдельно, тк должен быть класс кроме Main
-
-        class Sku {
-            String name; //название товара
-            double price; //цена товара
-            public static int goodsCount;
-
-            //теперь делаем  конструктор для товара
-            Sku(String nameSKU, double priceSKU){
-                name = nameSKU;
-                price = priceSKU;
-                goodsCount++;
-            }
-        }
-        */
 
         String end = "завершить";
         String text = "";
@@ -66,49 +43,34 @@ public class Main {
             list = list + "\n" + text;
             System.out.println(text + " - записали\n");
 
-            System.out.println("Цена, например, '150,10' - ");
-                if (scanner.hasNextDouble()) {
-                    pr = scanner.nextDouble();
-                        while (pr < 0) {
-                            System.out.println("!! Цена должна быть положительным числом. Укажите новую цену.");
-                            pr = 0.0;
-                            pr = scanner.nextDouble();
-                        }
-                    sum = sum + pr;
-                    System.out.println(pr + " - ок, посчитали\n");
-                 } else {
-                    System.out.println("!! Укажите цену цифрами, для разделения используй запятую.\nНазвание товара укажи заново.\n");
-                 }
+            do {
+                System.out.println("Укажите цену (больше нуля). Например, '150,10' - ");
+                while (!scanner.hasNextDouble()) {
+                    System.out.println("Не используйте буквы, указывайте запятую для нецелых чисел.");
+                    scanner.next();
+                }
+                pr = scanner.nextDouble();
+            } while (pr < 0.0);
+            System.out.println(pr + " - ок, посчитали\n");
+            sum += pr;
+
             Sku sku = new Sku(text, pr);
         }
 
-        if (list.equals("")) {
+        if (list.isEmpty()) {
             System.out.println("Ничего не заказано"); return;
         }
 
 
-            System.out.println("\n****\nСписок всех товаров:" + list.substring(0, list.length()) + "\n");
-            System.out.println("Общая сумма: " + sum + "   Количество человек: " + k + "\n");
+            System.out.println("\n*****************\nСписок всех товаров:" + list.substring(0, list.length()) + "\n\nОбщая сумма: " + sum + "   Количество человек: " + k + "\n");
             Double pay = sum / k;
             String payment = String.format("%.2f", pay);
             String rub = "";
 
-            switch (payment.substring(payment.length()-4,payment.length()-3)) {
-                case "1":
-                    rub = "рубль";
-                    break;
-                case "2":
-                case "3":
-                case "4":
-                    rub = "рубля";
-                    break;
-                default:
-                    rub = "рублей";
-                    break;
-            }
-
-
-            System.out.println("Сумма на каждого: " + payment + " " + rub + "\n****");
+            System.out.println("*****************\nСумма на каждого: ");
+            Rubl r = new Rubl(payment, pay);
+            r.rublCorrect(payment, pay, rub);
+            System.out.println("*****************");
 
 
     }
